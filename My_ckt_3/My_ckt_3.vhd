9d6fabb1e645f6711b16faab7388c8b354b4d056
Hane Liu <hane0131@gmail.com>
2015-12-05 01:36:36 +0800

    Declare entity

---
+++ My_ckt_3/My_ckt_3.vhd
@@ -0,0 +1,11 @@
+entity My_ckt_3 is
+port(
+    clk, Read, Write: in bit;
+    Data_Input: in bit_vector(7 downto 0);
+    Data_Output: out bit_vector(7 downto 0));
+end My_ckt_3;
+
+architecture MODEL of My_ckt_3 is
+begin
+
+end MODEL;
