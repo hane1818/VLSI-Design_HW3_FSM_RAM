a606e799833c7df175b1c05f98e50c6b7df4b444
Hane Liu <hane0131@gmail.com>
2015-12-13 01:36:43 +0800

    Set component & port map

--- My_ckt_3/My_ckt_3.vhd
+++ My_ckt_3/My_ckt_3.vhd
@@ -9,9 +9,24 @@ port(
 end My_ckt_3;
 
 architecture MODEL of My_ckt_3 is
+-- Component List --
+component Controller is
+port(
+    clk, Read, Write: in std_logic;
+    W_En: out std_logic;
+    R_Addr, W_Addr: out std_logic_vector(2 downto 0));
+end component Controller;
+component RAM is
+port(
+    clk, W_En: in std_logic;
+    W_Addr, R_Addr: in std_logic_vector(2 downto 0);
+    Data_Input: in std_logic_vector(7 downto 0);
+    Data_Output: out std_logic_vector(7 downto 0));
+end component RAM;
+-- Signal List --
 signal W_En: std_logic;
 signal R_Addr, W_Addr: std_logic_vector(2 downto 0);
-
 begin
-
+    C1: Controller port map (clk, Read, Write, W_En, R_Addr, W_Addr);
+    C2: RAM port map (clk, W_En, W_Addr, R_Addr, Data_Input, Data_Output);
 end MODEL;
