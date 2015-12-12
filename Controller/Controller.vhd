7c180fe260495ea2927060b8b578a78753e35088
Hane Liu <hane0131@gmail.com>
2015-12-12 23:46:27 +0800

    Add process of clk event

--- Controller/Controller.vhd
+++ Controller/Controller.vhd
@@ -9,7 +9,11 @@ architecture FSM of Controller is
 type state is (Idle, RState1, RState2, RState3, RState4, RState5, RState6, RState7, RState8,
                 WState1, WState2, WState3, WState4, WState5, WState6, WState7, WState8);
 signal current_stage, next_stage : state;
-
 begin
+    process(clk)
+    begin
+        if(clk'EVENT and clk='1')
+            current_stage <= next_stage;
+    end
 
 end FSM;
