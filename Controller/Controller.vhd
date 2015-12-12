8be17ff47372160e47bb0e9433905bd51a1347f9
Hane Liu <hane0131@gmail.com>
2015-12-13 00:14:32 +0800

    Set all read_state & fix syntax error

--- Controller/Controller.vhd
+++ Controller/Controller.vhd
@@ -8,46 +8,69 @@ end Controller;
 architecture FSM of Controller is
 type state_t is (Idle, RState1, RState2, RState3, RState4, RState5, RState6, RState7, RState8,
                 WState1, WState2, WState3, WState4, WState5, WState6, WState7, WState8);
-signal current_stage, next_stage : state_t;
+signal current_state, next_state : state_t;
 begin
     process(clk)
     begin
-        if(clk'EVENT and clk='1')
-            current_stage <= next_stage;
-    end
+        if(clk'EVENT and clk='1') then
+            current_state <= next_state;
+        end if;
+    end process;
 
-    process(current_stage)
+    process(current_state)
     begin
         R_Addr <= "000";
         W_Addr <= "000";
         W_En <= '0';
         
-        case current_stage is
+        case current_state is
             when Idle =>
                 if (Write='1') then
                     W_En <= '1';
-                    next_stage <= WState1;
+                    next_state <= WState1;
                 elsif (Read='1') then
-                    next_stage <= RState1;
+                    next_state <= RState1;
                 else
-                    next_stage <= Idle;
+                    next_state <= Idle;
                 end if;
                 
             when RState1 =>
+                next_state <= RState2;
             
             when RState2 =>
+                R_Addr <= "001";
+                
+                next_state <= RState3;
             
             when RState3 =>
+                R_Addr <= "010";
+                
+                next_state <= RState4;
             
             when RState4 =>
+                R_Addr <= "011";
+                
+                next_state <= RState5;
             
             when RState5 =>
+                R_Addr <= "100";
+                
+                next_state <= RState6;
             
             when RState6 =>
+                R_Addr <= "101";
+                
+                next_state <= RState7;
             
             when RState7 =>
+                R_Addr <= "110";
+                
+                next_state <= RState8;
             
             when RState8 =>
+                R_Addr <= "111";
+                
+                next_state <= Idle;
             
             when WState1 =>
             
@@ -64,6 +87,6 @@ begin
             when WState7 =>
             
             when WState8 =>
-            
-    end
+        end case;
+    end process;
 end FSM;
