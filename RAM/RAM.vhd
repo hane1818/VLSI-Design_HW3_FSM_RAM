078e9a449c11bf376c67539687071be2e98cd767
Hane Liu <hane0131@gmail.com>
2015-12-13 00:59:09 +0800

    Modify type & signal name and complete process

--- RAM/RAM.vhd
+++ RAM/RAM.vhd
@@ -7,8 +7,17 @@ port(
 end RAM;
 
 architecture MEMORY of RAM is
-type memory_t is array(7 downto 0) of bit_vector(7 downto 0);
-signal storage : memory_t;
+type storage is array(7 downto 0) of bit_vector(7 downto 0);
+signal Reg : storage;
 begin
-
+    process(clk)
+    begin
+        if(clk'EVENT and clk='1') then
+            Data_Output <= Reg(R_Addr);
+            
+            if(W_En='1') then
+                Reg(W_Addr) <= Data_Input;
+            end if;
+        end if;
+    end process;
 end MEMORY;
