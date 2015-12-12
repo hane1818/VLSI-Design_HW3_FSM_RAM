d9cd9f1d7577568f5953388c222767ae0f75bb26
Hane Liu <hane0131@gmail.com>
2015-12-12 23:46:59 +0800

    Modify self-define type name

--- Controller/Controller.vhd
+++ Controller/Controller.vhd
@@ -6,9 +6,9 @@ port(
 end Controller;
 
 architecture FSM of Controller is
-type state is (Idle, RState1, RState2, RState3, RState4, RState5, RState6, RState7, RState8,
+type state_t is (Idle, RState1, RState2, RState3, RState4, RState5, RState6, RState7, RState8,
                 WState1, WState2, WState3, WState4, WState5, WState6, WState7, WState8);
-signal current_stage, next_stage : state;
+signal current_stage, next_stage : state_t;
 begin
     process(clk)
     begin
